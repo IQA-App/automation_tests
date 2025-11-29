aa4a2b1a44567e07db85efec7e3569f2fb5935ce
Elgrueber <elenagrueber@gmail.com>
2025-11-29 17:15:38 -0500

    add api create user, login user, login admin tests

--- testData.js
+++ testData.js
@@ -1,16 +1,31 @@
 
 
-export const URL_ENDPOINT = {
+export const API_ENDPOINT = {
     getHealth: '/health', // GET Health check
-    login: '/api/auth/login', // POST Login
-    logout: '/api/auth/logout', // POST Logout
-    sendRequest: '/api/requests', // POST Request to create a new request
-    getRequest: '/api/status/:id', // GET Request status by ID
-    getSession: '/api/auth/me', // GET Session info
-    getRequestsList: '/api/requests', // GET Requests list
-    getRequestById: (id) => `/api/requests/${id}`, // GET Request by ID
-    createRequest: '/api/requests', // POST to create a new request
-    updateRequest: (id) => `/api/requests/${id}`, // PATCH to update a request
-    deleteRequest: (id) => `/api/requests/${id}`, // DELETE to delete a request
-    sendRequest: '/api/requests', // POST Request to create a new request
+    createUser: '/user', // POST Create user
+    login: '/auth/login', // POST Login
+    logout: '/auth/logout', // POST Logout
+    sendRequest: '/requests', // POST Request to create a new request
+    getRequest: '/status/:id', // GET Request status by ID
+    getSession: '/auth/me', // GET Session info
+    getRequestsList: '/requests', // GET Requests list
+    getRequestById: (id) => `/requests/${id}`, // GET Request by ID
+    createRequest: '/requests', // POST to create a new request
+    updateRequest: (id) => `/requests/${id}`, // PATCH to update a request
+    deleteRequest: (id) => `/requests/${id}`, // DELETE to delete a request
+    sendRequest: '/requests', // POST Request to create a new request
+};
+
+const randNum = () => Math.floor(Math.random() * 900000) + 100000; // 6-digit random
+const makeEmail = (prefix = 'userU') => `${prefix}${randNum()}@example.com`;
+
+export const CREDS = {
+    admin: {
+        email: "admin@test.com",
+        password: "123456qQ!"
+    },
+    user: {
+        email: `test${randNum()}@example.com`, 
+        password: "123456Qq!"
+    }
 };
